c3e9dadb994b0c4db2bbf6a317591f34c0d0f7f5
dennysinurat <74443498+dennysinurat@users.noreply.github.com>
2020-11-14 14:24:18 +0700

    Update multithread_statistika.c

--- multithread_statistika.c
+++ multithread_statistika.c
@@ -8,6 +8,16 @@ int n, i;
 double avg;
 int min, max;
 
+// Menghitung nilai rata-rata
+void *average(void *arg) {
+    for(i=0; i<n; i++)
+        sum = sum + arr[i];
+    avg = sum / n;
+  
+    printf("The average value is %.0lf\n", avg);
+    pthread_exit(NULL);
+}
+
 // Menghitung nilai minimum
 void *minimum(void *arg) {
     min = arr[0];
